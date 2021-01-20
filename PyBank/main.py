88331a37ac23ff5d2bfba2bd5bf1ba750fb935aa
Corwin Galleries <corwingalleries@Corwins-iMac.domain>
2021-01-19 21:51:20 -0700

    added csvreader and code to total the number of months in CSV. Also commented future sections of assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,4 +5,34 @@ import os
 import csv
 
 
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "budget_data.csv")
+print(csvpath)
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter= ",")
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    #count total number of months in CSV
+    count = len(list(csvreader))
+    print(count)
+     
+
+     #net total amount of Profit/Losses over the entire period
+
+
+     #Changes in Profit/Losses over entire period 
+
+
+
+     #Averages of changes
+
+
+     #greatest increase in profits (date & amount) over entire period
+
+     #greates decrease in losses (date and amount) over entire period
+
+     
