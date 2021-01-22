809879b4e4d6484b30ab74caa1a4d1a919884ce0
Ciera Morris <cieranmorris@gmail.com>
2021-01-21 22:09:37 -0700

    import modules and csvreaders, set up for total votes cast

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,36 @@
 print("Hello from PyPoll!")
 
-#import module and csv reader
+#import modules
 import os
 import csv
+import statistics as avg
+
+#create path to csv file and rename
+election_data_csv = os.path.join("Resources", "election_data.csv")
+print(election_data_csv)
+
+#Lists to store data to analyze later
+total_votes_cast = []
+candidate_list = []
+candidate_votes = []
+popular_vote = []
+
+#csvreader to read csv file
+election_data = csv.reader(election_data_csv, delimiter = ",")
+print(election_data)
+
+#skip header row in csv file
+csv_header = next(election_data)
+print(f"CSV Header: {csv_header}")
+
+
+#total number of votes cast - add up # of voter IDs
+#need to convert voter IDs into individual numerical values?
+total_votes_cast = 0
+for row in election_data:
+    total_votes_cast = total_votes_cast + 1
+    print(total_votes_cast)
+
+#not correct - indicating there were only 26 votes
+#do something in regards to totaling all values in column 0
+#append to lists tomorrow
